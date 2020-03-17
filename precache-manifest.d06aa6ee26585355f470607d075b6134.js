862128d991fd498422a4843adbd6b56bdf79c3d6
MisiekKisiek <skrzypkka@gmail.com>
2020-03-18 00:46:11 +0100

    Updates

---
+++ precache-manifest.d06aa6ee26585355f470607d075b6134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb6283286b97bbf5e106525b709889b",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "fcf1d7a434b27aa56638",
+    "url": "/toDoList/static/css/main.6fee60c5.chunk.css"
+  },
+  {
+    "revision": "b7afa2a271cb4cc87349",
+    "url": "/toDoList/static/js/2.a3aff292.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/toDoList/static/js/2.a3aff292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf1d7a434b27aa56638",
+    "url": "/toDoList/static/js/main.62a71cd6.chunk.js"
+  },
+  {
+    "revision": "cc563ba775fc5f4e3994",
+    "url": "/toDoList/static/js/runtime-main.b9339a71.js"
+  },
+  {
+    "revision": "76f72909cd67478a488a7a8d12926a64",
+    "url": "/toDoList/static/media/logo.76f72909.png"
+  }
+]);
